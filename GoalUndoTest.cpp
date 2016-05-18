063ceb24309e95934643ee0f9a14d54e0b977158
Arjun Arun Kumar <aarunkumar@mail.csuchico.edu>
2016-05-17 21:39:50 -0700

    Test cases written thoroughly

--- GoalUndoTest.cpp
+++ GoalUndoTest.cpp
@@ -4,7 +4,7 @@
 
 #include <gtest/gtest.h>
 #include "GoalUndo.h"
- 
+
 class GoalUndoTest : public ::testing::Test
 {
 	protected:
@@ -17,4 +17,60 @@ class GoalUndoTest : public ::testing::Test
 TEST(GoalUndoTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
+}
+
+TEST(GoalUndoTest , addGoaloperation)
+{
+	GoalUndo goal;
+	goal.addOperation("square","perimeter");
+	goal.addOperation("Area");
+	ASSERT_EQ("square",goal.getGoal());
+	goal.undoOperation();
+	goal.undoOperation("perimeter");
+	ASSERT_EQ("",goal.getOperations());
+	}
+
+TEST(GoalUndoTest, addGoaloperation1)
+{
+	GoalUndo goal;
+	goal.undoOperation();
+	ASSERT_EQ("",goal.getOperations());
+}
+
+
+TEST(GoalUndoTest, addGoaloperation3)
+{
+	GoalUndo goal;
+	goal.addOperation("rectangle","height");
+	goal.addOperation("triangle","3sides");
+	goal.undoGoal();
+	ASSERT_EQ("rectangle",goal.getGoal());
+	goal.undoGoal();
+	ASSERT_EQ("",goal.getGoal());
+}
+
+TEST(GoalUndoTest, CheckemptyORpop)
+{
+  GoalUndo goal;
+  goal.undoGoal();
+  ASSERT_EQ("",goal.getGoal());
+}
+
+
+TEST(GoalUndoTest, CheckemptyORpop1)
+{
+	GoalUndo goal;
+	goal.addOperation("diameter");
+	ASSERT_EQ("diameter",goal.getOperations());
+	goal.undoOperation();
+	goal.undoOperation();
+	ASSERT_EQ("",goal.getOperations());
+}
+
+TEST(GoalUndoTest, CheckemptyORpop2)
+{
+	GoalUndo goal;
+	goal.addOperation("circle","radius");
+	goal.addOperation("surfacearea");
+	ASSERT_EQ("radius surfacearea",goal.getOperations());
 }
